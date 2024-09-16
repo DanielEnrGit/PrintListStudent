372e9f40952d51ed6fc6dd792326d2ef1e5298b3
DanielEnrGit <enriquda@sheridancollege.ca>
2024-09-16 18:07:26 -0400

    studentlist version 1

---
+++ src/printstudents/Student.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package printstudents;
+
+/**
+ *
+ * @author enriq
+ */
+public class Student {
+    //data member - raw data for student entity
+    //model student details
+    private String Name; //name private
+    private int id; //encapsulation
+
+    /**
+     * @return the Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    /**
+     * @param Name the Name to set
+     */
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
